8211d5fd02be01a5bde897b53cf1763a422928c0
Rubens Gomes <vitorr_gomes@hotmail.com>
2017-10-05 00:09:12 -0300

    start changing messages

--- src/servidor.java
+++ src/servidor.java
@@ -18,7 +18,7 @@ import java.util.Vector;
 
 
 public class Servidor extends Thread {
-    // Parte que controla as conexões por meio de threads.
+    // Parte que controla as conexï¿½es por meio de threads.
     private static Vector CLIENTES;
     // socket deste cliente
     private Socket conexao;
@@ -30,18 +30,18 @@ public class Servidor extends Thread {
     public Servidor(Socket socket) {
         this.conexao = socket;
     }
-    //testa se nomes são iguais, se for retorna true
+    //testa se nomes sï¿½o iguais, se for retorna true
     public boolean armazena(String newName){
     //   System.out.println(LISTA_DE_NOMES);
        for (int i=0; i < LISTA_DE_NOMES.size(); i++){
          if(LISTA_DE_NOMES.get(i).equals(newName))
            return true;
        }
-       //adiciona na lista apenas se não existir
+       //adiciona na lista apenas se nï¿½o existir
        LISTA_DE_NOMES.add(newName);
        return false;
     }
-    //remove da lista os CLIENTES que já deixaram o chat
+    //remove da lista os CLIENTES que jï¿½ deixaram o chat
     public void remove(String oldName) {
        for (int i=0; i< LISTA_DE_NOMES.size(); i++){
          if(LISTA_DE_NOMES.get(i).equals(oldName))
@@ -58,35 +58,35 @@ public class Servidor extends Thread {
             // Loop principal.
             while (true) {
                 // aguarda algum cliente se conectar.
-                // A execução do servidor fica bloqueada na chamada do método accept da
-                // classe ServerSocket até que algum cliente se conecte ao servidor.
-                // O próprio método desbloqueia e retorna com um objeto da classe Socket
+                // A execuï¿½ï¿½o do servidor fica bloqueada na chamada do mï¿½todo accept da
+                // classe ServerSocket atï¿½ que algum cliente se conecte ao servidor.
+                // O prï¿½prio mï¿½todo desbloqueia e retorna com um objeto da classe Socket
                 Socket conexao = server.accept();
-                // cria uma nova thread para tratar essa conexão
+                // cria uma nova thread para tratar essa conexï¿½o
                 Thread t = new Servidor(conexao);
                 t.start();
-                // voltando ao loop, esperando mais alguém se conectar.
+                // voltando ao loop, esperando mais alguï¿½m se conectar.
             }
         } catch (IOException e) {
-            // caso ocorra alguma excessão de E/S, mostre qual foi.
+            // caso ocorra alguma excessï¿½o de E/S, mostre qual foi.
             System.out.println("IOException: " + e);
         }
     }
-    // execução da thread
+    // execuï¿½ï¿½o da thread
     public void run()
     {
         try {
-            // objetos que permitem controlar fluxo de comunicação que vem do cliente
-            BufferedReader entrada = 
+            // objetos que permitem controlar fluxo de comunicaï¿½ï¿½o que vem do cliente
+            BufferedReader entrada =
 				new BufferedReader(new InputStreamReader(this.conexao.getInputStream()));
-            
+
 			PrintStream saida = new PrintStream(this.conexao.getOutputStream());
             // recebe o nome do cliente
             this.nomeCliente = entrada.readLine();
-            
-            
-            System.out.println (this.conexao.getRemoteSocketAddress().toString());    
-            
+
+
+            System.out.println (this.conexao.getRemoteSocketAddress().toString());
+
             //chamada ao metodo que testa nomes iguais
             if (armazena(this.nomeCliente)){
               saida.println("Este nome ja existe! Conecte novamente com outro Nome.");
@@ -98,7 +98,7 @@ public class Servidor extends Thread {
                //mostra o nome do cliente conectado ao servidor
                System.out.println(this.nomeCliente + " : Conectado ao Servidor!");
             }
-//            //igual a null encerra a execução
+//            //igual a null encerra a execuï¿½ï¿½o
             if (this.nomeCliente == null) {
                 return;
             }
@@ -106,22 +106,32 @@ public class Servidor extends Thread {
             CLIENTES.add(saida);
             //recebe a mensagem do cliente
             String msg = entrada.readLine();
-            // Verificar se linha é null (conexão encerrada)
-            // Se não for nula, mostra a troca de mensagens entre os CLIENTES
+            // Verificar se linha ï¿½ null (conexï¿½o encerrada)
+            // Se nï¿½o for nula, mostra a troca de mensagens entre os CLIENTES
             while (msg != null && !(msg.trim().equals("")))
             {
+
+
+                /** Verificar se representa um comando da aplicacao
+                 *  !play id
+                 *  !players
+                 *  !status
+                 */
+
+                userCommandsActions(saida, msg, );
+
                 // reenvia a linha para todos os CLIENTES conectados
                 sendToAll(saida, " escreveu: ", msg);
-                
+
                 sendToAll(saida, "asdfg#",this.conexao.getRemoteSocketAddress().toString());
-                
-               
+
+
                 // espera por uma nova linha.
                 msg = entrada.readLine();
             }
             //se cliente enviar linha em branco, mostra a saida no servidor
             System.out.println(this.nomeCliente + " saiu do bate-papo!");
-            //se cliente enviar linha em branco, servidor envia 
+            //se cliente enviar linha em branco, servidor envia
 			// mensagem de saida do chat aos CLIENTES conectados
             sendToAll(saida, " saiu", " do bate-papo!");
             //remove nome da lista
@@ -131,23 +141,35 @@ public class Servidor extends Thread {
             //fecha a conexao com este cliente
             this.conexao.close();
         } catch (IOException e) {
-            // Caso ocorra alguma excessão de E/S, mostre qual foi.
+            // Caso ocorra alguma excessï¿½o de E/S, mostre qual foi.
             System.out.println("Falha na Conexao... .. ."+" IOException: " + e);
         }
     }
-    // enviar uma mensagem para todos, menos para o próprio
+    // enviar uma mensagem para todos, menos para o prï¿½prio
     public void sendToAll(PrintStream saida, String acao, String msg) throws IOException {
         Enumeration e = CLIENTES.elements();
         while (e.hasMoreElements()) {
-            // obtém o fluxo de saída de um dos CLIENTES
+            // obtï¿½m o fluxo de saï¿½da de um dos CLIENTES
             PrintStream chat = (PrintStream) e.nextElement();
-            // envia para todos, menos para o próprio usuário
+            // envia para todos, menos para o prï¿½prio usuï¿½rio
             if (chat != saida) {
                 chat.println(this.nomeCliente + acao + msg);
             }
         }
       }
-    
-    
-    
+
+
+    // Mapeamento de comandos
+    public void userCommandsActions(PrintStream saida, String acao, String msg) throws IOException {
+        Enumeration e = CLIENTES.elements();
+        while (e.hasMoreElements()) {
+            // obtï¿½m o fluxo de saï¿½da de um dos CLIENTES
+            PrintStream chat = (PrintStream) e.nextElement();
+            // envia para todos, menos para o prï¿½prio usuï¿½rio
+            if (chat != saida) {
+                chat.println(this.nomeCliente + acao + msg);
+            }
+        }
+      }
+
 }
